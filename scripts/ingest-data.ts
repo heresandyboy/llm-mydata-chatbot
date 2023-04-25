b6a73f1fcb50d65c7f716cc9082fa5d848af7f0b
Andy Ainsworth <heresandyboy@gmail.com>
2023-04-25 13:20:36 +0100

    ingest-data selects vector store based on env vars

--- scripts/ingest-data.ts
+++ scripts/ingest-data.ts
@@ -1,47 +1,24 @@
-import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
-import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
-import { PineconeStore } from 'langchain/vectorstores/pinecone';
-import { pinecone } from '@/utils/pinecone-client';
-import { CustomPDFLoader } from '@/utils/customPDFLoader';
-import { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE } from '@/config/pinecone';
-import { DirectoryLoader } from 'langchain/document_loaders/fs/directory';
+import { run as runPinecone } from './ingest-data-pinecone';
+import { run as runWeaviate } from './ingest-data-weaviate';
 
-/* Name of directory to retrieve your files from */
-const filePath = 'docs';
+const usePinecone = process.env.USE_PINECONE === 'true';
+const useWeaviate = process.env.USE_WEAVIATE === 'true';
 
-export const run = async () => {
-  try {
-    /*load raw docs from the all files in the directory */
-    const directoryLoader = new DirectoryLoader(filePath, {
-      '.pdf': (path) => new CustomPDFLoader(path),
-    });
+if (usePinecone && useWeaviate) {
+  throw new Error(
+    'Please set only one of USE_PINECONE or USE_WEAVIATE to true',
+  );
+}
 
-    // const loader = new PDFLoader(filePath);
-    const rawDocs = await directoryLoader.load();
+if (!usePinecone && !useWeaviate) {
+  throw new Error('Please set one of USE_PINECONE or USE_WEAVIATE to true');
+}
 
-    /* Split text into chunks */
-    const textSplitter = new RecursiveCharacterTextSplitter({
-      chunkSize: 1000,
-      chunkOverlap: 200,
-    });
-
-    const docs = await textSplitter.splitDocuments(rawDocs);
-    console.log('split docs', docs);
-
-    console.log('creating vector store...');
-    /*create and store the embeddings in the vectorStore*/
-    const embeddings = new OpenAIEmbeddings();
-    const index = pinecone.Index(PINECONE_INDEX_NAME); //change to your own index name
-
-    //embed the PDF documents
-    await PineconeStore.fromDocuments(docs, embeddings, {
-      pineconeIndex: index,
-      namespace: PINECONE_NAME_SPACE,
-      textKey: 'text',
-    });
-  } catch (error) {
-    console.log('error', error);
-    throw new Error('Failed to ingest your data');
+const run = async () => {
+  if (usePinecone) {
+    await runPinecone();
+  } else if (useWeaviate) {
+    await runWeaviate();
   }
 };
 
